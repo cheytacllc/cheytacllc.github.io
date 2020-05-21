94e85507be1a4b66ea1b480018123eec5a3e4cc9
mguludag <m231@tnctr.com>
2020-05-22 01:49:57 +0300

    Site updated: 2020-05-22 01:49:57

--- sw.js
+++ sw.js
@@ -24,13 +24,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2020/05/21/Github-Pages-Uzerine-Hexo-Kurulumu/');
-
   precacheUrls.push('/2019/11/17/C-Stili-Dizilerden-std-array-e/');
 
-  precacheUrls.push('/2020/03/09/cisco_router_routing/');
+  precacheUrls.push('/2020/05/21/Github-Pages-Uzerine-Hexo-Kurulumu/');
 
   precacheUrls.push('/2020/05/12/OpenCV-DNN-ile-Daha-Iyi-Kenar-Belirleme/');
+
+  precacheUrls.push('/2020/03/09/cisco_router_routing/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
